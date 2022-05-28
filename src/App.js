2fd10fe2145cc22c1d97b21a0db26052533f3a95
skdding3 <skdding3@gmail.com>
2022-05-28 11:25:29 +0900

    Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Hookprac1 from "./components/Hookprac1";
-import Hookprac2 from "./components/Hookprac2";
-import Hookprac3 from "./components/Hookprac3";
-import Hookprac4 from "./components/Hookprac4";
-import Hookprac5 from "./components/Hookprac5";
+import useWindowWidth from "./hooks/useWindowWidth";
 
 function App() {
+  const width = useWindowWidth();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hookprac1 />
-        <Hookprac2 />
-        <Hookprac3 />
-        <Hookprac4 />
-        <Hookprac5 />
+        {width}
       </header>
     </div>
   );
